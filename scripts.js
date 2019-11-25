5212dacb4897eb099fccd1f6473ec3dcfddb8dde
kcc165 <kcc165@msstate.edu>
2019-11-25 17:06:56 -0600

    Start checkWin function

--- scripts.js
+++ scripts.js
@@ -9,13 +9,22 @@ const newPlayer = (name, piece) => {
 
 const gameBoard = () => {
     let board = ["", "", "", "", "", "", "", "", ""];
+    let winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     let currentPlayer;
-    const setPiece = (index) => {
+    const setPiece = (index, piece) => {
         if (board[index] === ""){
             alert("Space occupied. Choose another space");
         }
         else{
-            board[index] = currentPlayer.getPiece();
+            board[index] = piece;
         }
+    }
+    const checkWin = () => {
+        winConditions.forEach((condition) => {
+            if (board[condition[0]] === board[condition[1]] &&
+                board[condition[1]] === board[condition[2]]){
+
+                }
+        })
     }
 }
